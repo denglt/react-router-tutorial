87d4de39177a8ea079db245fb70a94ae48a4cfb1
denglt <ltdeng@hotmail.com>
2017-10-24 17:17:32 +0800

    no

--- lessons/06-params/modules/Repo.js
+++ lessons/06-params/modules/Repo.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 export default React.createClass({
   render() {
-    console.log('Repo render')
+
+    console.log('Repo render');
+    console.log(this.props);
+    console.log(this.props.params);
     return (
           <div>
                <h2>Repo</h2>
+               <h3>{this.props.params.userName}</h3>
                <h3>{this.props.params.repoName}</h3>
           </div>
       );
